2d06df7eae9adb9151f93d608afd50caa34bc470
SnowBit <dtshah24022006@gmail.com>
2022-04-17 22:17:00 +0530

    Add favicon

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,16 @@ export default function Home() {
   })
   return (
     <div className="dark:bg-black dark:text-white duration-300">
+      <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <NextSeo
         title="Watch Tab"
         description="Watch Tab is a simple and easy to use digital clock. It displays the current time in an elegant minimalist design and it is completely ad-free."
+        canonical="https://watch-tab.now.sh/"
     />
       <div className="flex justify-center items-center h-screen">
         <h1 className="text-[19vw] font-black" suppressHydrationWarning>
